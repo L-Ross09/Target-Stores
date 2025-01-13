4b2645fa32c50cb358cd72f91210bc5192dcd138
Chase-Gavin <cgavin28@students.stab.org>
2025-01-13 14:55:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Miscellaneous/Target%20Store%20Locations.csv"
+var address = getColumn(url, 1);
+var city = getColumn(url, 2);
+var county = getColumn(url,3);
+var state = getColumn(url, 8);
 
+console.log(address)
