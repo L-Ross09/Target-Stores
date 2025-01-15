752ffb00b38c3018352dc6a300cdf1fcec636c1f
Chase-Gavin <cgavin28@students.stab.org>
2025-01-15 09:55:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,33 @@ var city = getColumn(url, 2);
 var county = getColumn(url,3);
 var state = getColumn(url, 8);
 
-console.log(address)
+// Function to get a list of unique states
+function getStatesWithTargets() {
+    var uniqueState = [];
+    for (var i = 0; i < state.length; i++) {
+        if (!uniqueState.includes(state[i])) {
+            uniqueState.push(state[i]);
+        }
+    }
+    return uniqueState;
+}
+console.log(getStatesWithTargets())
+
+function howManyTargetsInAState(stateName) {
+    // Count the number of targets in the given state (stateName)
+    var count = 0;
+    var wasMatch = false;
+    for (var i = 0; i < state.length; i++) {
+        if (state[i] == stateName) {
+            count += 1;  // Inplement the count when the state matches
+            wasMatch= true;
+        }
+
+    }
+    if (wasMatch == false) {
+        return -1;  
+    }
+
+    return count;  // Return the count of targets for the given state
+}
+console.log(howManyTargetsInAState("maylnd"));
