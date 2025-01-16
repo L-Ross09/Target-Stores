e06338dcac29783b83392d223a89780ff43b4875
L-Ross09 <lbross126@gmail.com>
2025-01-16 17:20:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ var state = getColumn(url, 8);
 
 // Function to get a list of unique states
 function getStatesWithTargets() {
-    var uniqueState = [];
-    for (var i = 0; i < state.length; i++) {
-        if (!uniqueState.includes(state[i])) {
-            uniqueState.push(state[i]);
+    var uniqueStates = [];
+    for (var i = 0; i < states.length; i++) {
+        if (!uniqueStates.includes(states[i])) {
+            uniqueStates.push(states[i]);
         }
     }
-    return uniqueState;
+    return uniqueStates;
 }
 console.log(getStatesWithTargets())
 
@@ -20,8 +20,8 @@ function howManyTargetsInAState(stateName) {
     // Count the number of targets in the given state (stateName)
     var count = 0;
     var wasMatch = false;
-    for (var i = 0; i < state.length; i++) {
-        if (state[i] == stateName) {
+    for (var i = 0; i < states.length; i++) {
+        if (states[i] == statesName) {
             count += 1;  // Inplement the count when the state matches
             wasMatch= true;
         }
